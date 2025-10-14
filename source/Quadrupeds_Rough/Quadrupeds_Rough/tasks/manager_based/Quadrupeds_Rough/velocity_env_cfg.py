22459e2cbf9767d27a46af5be480f79345b85ad6
Leo Wang <84789689+leo01110111@users.noreply.github.com>
2025-10-14 15:29:34 -0700

    oops ignore the last commit

--- source/Quadrupeds_Rough/Quadrupeds_Rough/tasks/manager_based/Quadrupeds_Rough/velocity_env_cfg.py
+++ source/Quadrupeds_Rough/Quadrupeds_Rough/tasks/manager_based/Quadrupeds_Rough/velocity_env_cfg.py
@@ -135,7 +135,7 @@ class ObservationsCfg:
             func=mdp.height_scan,
             params={"sensor_cfg": SceneEntityCfg("height_scanner")},
             noise=Unoise(n_min=-0.1, n_max=0.1),
-            clip=(-1.0, 1.0),
+            clip=(-1.0, 1.0) , 
         )"""
 
         def __post_init__(self):
